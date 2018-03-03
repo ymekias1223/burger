bd551a47af196ae77d8492b73388fc1cfee453f8
Your Name <you@example.com>
2018-03-02 16:05:10 -0800

    first commit, most functions work

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('mushroom');
+INSERT INTO burgers (name) VALUES ('steak');
+INSERT INTO burgers (name) VALUES ('double cheese');
+INSERT INTO burgers (name) VALUES ('veggie');
+INSERT INTO burgers (name) VALUES ('hawaian');
+INSERT INTO burgers (name) VALUES ('Bacon');
